33c4c503c5f0eba80a208f3aaf6c0159488c3cf2
David Smerkous <smerkousdavid@gmail.com>
2016-01-30 11:46:18 -0600

    Turret no need for horizontal

--- src/org/usfirst/frc/team5431/robot/subsystems/Turret_Subsystem.java
+++ src/org/usfirst/frc/team5431/robot/subsystems/Turret_Subsystem.java
@@ -10,23 +10,24 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 public class Turret_Subsystem extends Subsystem {
 
 	
-	private CANTalon horiz, left, right;
+	//private CANTalon horiz, left, right;
+	private CANTalon left, right;
 	private static final Turret_Subsystem INSTANCE = new Turret_Subsystem();
 	
 	
 	public Turret_Subsystem()
 	{
 		super();
-		this.horiz = new CANTalon(RobotMap.TurnBase); // Horizontal turning motor
+		//this.horiz = new CANTalon(RobotMap.TurnBase); // Horizontal turning motor
 		this.left = new CANTalon(RobotMap.LeftFly); // Left flywheel motor
 		this.right = new CANTalon(RobotMap.RightFly); // Right flywheel motor
-		this.horiz.enable(); // Automatically called in init, but why not call it again just to make sure
+		//this.horiz.enable(); // Automatically called in init, but why not call it again just to make sure
 		this.left.enable();
 		this.right.enable();
 		this.left.clearStickyFaults(); // Clears any values inside talon from previous executions
 		this.right.clearStickyFaults();
-		this.horiz.clearStickyFaults();
-		this.horiz.enableBrakeMode(true); // Brake when no signal received. Prevent backdrive
+		//this.horiz.clearStickyFaults();
+		//this.horiz.enableBrakeMode(true); // Brake when no signal received. Prevent backdrive
 		this.left.enableBrakeMode(false);
 		this.right.enableBrakeMode(false);
 	}
@@ -38,17 +39,17 @@ public class Turret_Subsystem extends Subsystem {
 	
 	public void turnLeft(double amount)
 	{
-		this.horiz.set(amount); // Turn left at this power
+		//this.horiz.set(amount); // Turn left at this power
 	}
 	
 	public void turnRight(double amount)
 	{
-		this.horiz.set(-amount); // Turn right at this power
+		//this.horiz.set(-amount); // Turn right at this power
 	}
 	
 	public void stopTurn()
 	{
-		this.horiz.set(0); // Brake
+		//this.horiz.set(0); // Brake
 		
 	}
 	
